9dbd1514dae67404b336e661d81aa397e9aee98a
ravi.kumar <ravi.kumar@planview.com>
2023-11-09 14:43:38 +0530

    additon

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -2,6 +2,13 @@
 {
   public class Class1
   {
-
+    public int Addition(int a, int b)
+    {
+      return a + b;
+    }
+     public int Substraction(int a,int b)
+     {
+       return a - b;
+     }
   }
 }
